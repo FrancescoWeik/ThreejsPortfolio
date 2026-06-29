e8dd29e4af3178afd69d2fbc1dd4f799ae3d09c6
FrancescoWeikmann <fweikmann@gmail.com>
2026-06-29 10:24:56 +0200

    fix members description and add english button

--- src/Experience/ducksMembers.js
+++ src/Experience/ducksMembers.js
@@ -4,56 +4,56 @@ export default [
     {
         node: 'Arri',
         name: 'Arri',
-        description: 'Designer',
+        description: "Not just a designer, the biggest designer of 'em all!",
         link: 'https://www.linkedin.com/in/arrigo-berton/',
         imagePath: 'textures/DuckImages/Placeholder.png'
     },
     {
         node: 'Davide',
         name: 'Davide',
-        description: 'Artist',
+        description: "Artist made of iron. Or iron in the form of artist. Or duck, I don't know",
         link: 'https://www.artstation.com/davide_ferro',
         imagePath: 'textures/DuckImages/Placeholder.png'
     },
     {
         node: 'Fede',
         name: 'Fede',
-        description: 'Composer',
+        description: 'When he speaks the only thing you hear is music... Such is the way of a true composer',
         link: 'https://www.linkedin.com/in/federico-mannino/.com',
         imagePath: 'textures/DuckImages/Placeholder.png'
     },
     {
         node: 'Fra',
         name: 'Fra',
-        description: 'Developer',
+        description: "Developer aiming to be a steampunk developer. Does that mean anything? Probably not",
         link: 'https://francesco-portfolio.vercel.app/',
         imagePath: 'textures/DuckImages/Placeholder.png'
     },
     {
         node: 'Henry',
         name: 'Henry',
-        description: 'Artist',
+        description: "Not even the big one can fathom his artistic skills. That's our Henry for you",
         link: 'https://www.artstation.com/henrycanes',
         imagePath: 'textures/DuckImages/Placeholder.png'
     },
     {
         node: 'Lore',
         name: 'Lore',
-        description: 'Artist',
+        description: 'Art is just the voice of his hands. Characters dance hearing the sound of his pencil',
         link: 'Lore Link .com',
         imagePath: 'textures/DuckImages/Placeholder.png'
     },
     {
         node: 'Lucas',
         name: 'Lucas',
-        description: 'Developer',
+        description: "Need organization? Need to deliver on time? This developer can lead to anything you want!",
         link: 'https://www.linkedin.com/in/lucas-calz%C3%A0/',
         imagePath: 'textures/DuckImages/Placeholder.png'
     },
     {
         node: 'Teo',
         name: 'Teo',
-        description: 'Developer',
+        description: "Hey there's a problem! Don't worry, Teo is there to fix anything! Such is the way of this developer",
         link: 'https://www.linkedin.com/in/matteo-pisani-a41555157/.com',
         imagePath: 'textures/DuckImages/Placeholder.png'
     },
